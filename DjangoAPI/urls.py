36e412d948dfb211cce8d79e97bae5ee4f2372ee
HsiehMingHsuan <ted0104@icloud.com>
2021-10-27 14:45:58 +0800

    use path method instead of url

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('TestApp.urls'))
+    path('', include('TestApp.urls'))
 ]
--- TestApp/urls.py
+++ TestApp/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url
-from django.urls.resolvers import URLPattern
+from django.urls import path
 from TestApp import views
 
 
 urlpatterns = [
-    url(r'^table1$', views.table1Api),
-    url(r'^table1/([0-9]+)$',views.table1Api)
+    path('', views.table1Api),
 ]
