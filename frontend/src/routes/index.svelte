f56b8f776989707133220aaea2ac263032a3971f
HsiehMingHsuan <ted0104@icloud.com>
2021-11-09 20:38:49 +0800

    svelte kit

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Nav from "../components/nav.svelte"
+    const a = 'test';
+</script>
+<svelte:head>
+    <title>SWE Explore</title>
+</svelte:head>
+<h1>Software Engineer Explore</h1>
+<p>{a}</p>
+<Nav/>
+<style>
+    h1{
+        color: crimson;
+    }
+</style>
