e131c1b3a1ab76f86c009716d65808b12b16145a
HsiehMingHsuan <ted0104@icloud.com>
2021-11-18 13:53:42 +0800

    new page

---
+++ frontend_svelte/src/routes/company.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>SWE Explore: Company</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">SWE Explore</h1>
+<a href="http://localhost:3000">This is about page</a>
